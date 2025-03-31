a21ce1ef48a338d6444ac5c7b60b15ce0628bcf8
Brandon Guerra <guerrab@proactrx.com>
2025-03-31 11:57:49 -0400

    Added streamlit as the GUI for the application

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 import pandas as pd
+import streamlit as st
 from datetime import date, timedelta
-
-#generate excel file depending on user input 
-#need to generate 7 columns, and 30 rows
-#date, start time, end time, time spent, ticket, category, notes (columns)
-
-#pandas is a good library to use
-xlsx_file_path = 'eggs.xlsx'
-
-df = pd.read_excel("eggs.xlsx", sheet_name="eggs")
-
-#build dict and then pass it to data frame
-
-dates = []
-start_times=[]
-end_times=[]
-time_spent=[]
-ticket=[]
-category=[]
-notes=[]
-
 
 
 current_date = date.today()
 
-for i in range(5):
-    current_date -= timedelta(days=1)
-    dates.append(current_date.strftime("%m/%d/%Y"))
+# for i in range(5):
+#     current_date -= timedelta(days=1)
+#     dates.append(str(current_date.strftime("%m/%d/%Y")))
 
-dates.reverse()
+# dates.reverse()
 
-d = {}
+# date_keys = ['1','2','3','4','5']
+# date_values = dates
 
-df = pd.DataFrame(data=d)
+# d = {date_keys[i]:date_values[i] for i in range(len(date_keys))}
 
-df.to_excel(xlsx_file_path, index = False, sheet_name="eggs")
+
+with open("test.txt", 'w') as f:
+    f.write('This is a new text file!')
+
+df = pd.DataFrame(
+        {"Dates":["2/1/25"],
+        "Start Time":["8:00am"],
+        "End Time":["10:00pm"],
+        "Time Spent":["2 Hours"],
+        "Ticket":["NA"],
+        "Category":["NA"],
+        "Notes":["Notes go here"]
+    }
+)
+ 
+
+st.dataframe(df, hide_index=True)
+st.download_button(label="Download Log as .xlsx file",data="test.txt",file_name="downloaded_file.txt")
+
+text = st.text_area("Text Field")
 
 
 
